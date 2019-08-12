88f5cc3282abe5df8b6459904947ab811909820e
vinilitaiff <vinilitaiff@gmail.com>
2019-08-12 19:33:38 -0400

    tela de home e cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis Top</strong>
+              <span>R$431</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={16} color="#de1a22" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={16} color="#de1a22" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$215,5</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={16} color="#de1a22" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1231,41</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://www.flightclub.com/media/catalog/product/cache/1/small_image/360x257/9df78eab33525d08d6e5fb8d27136e95/1/4/143598_01.jpg"
+          alt="Yezzy"
+        />
+        <strong>Descricao</strong>
+        <span>R$412</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
