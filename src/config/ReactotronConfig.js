676a810dd7644d12cd42b7588921c24d738a0176
vinilitaiff <vinilitaiff@gmail.com>
2019-08-15 23:37:34 -0400

    calculo de totais carrinho

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -4,6 +4,7 @@ import { reactotronRedux } from 'reactotron-redux';
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .use(reactotronRedux())
+    .configure({ host: '192.168.0.16' })
     .connect();
 
   tron.clear();
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -31,7 +31,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
-
+    const { amount } = this.props;
     return (
       <ProductList>
         {products.map(product => (
@@ -44,7 +44,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" />3
+                <MdAddShoppingCart size={16} color="#fff" />
+                {amount[product.id] || 0}
               </div>
 
               <span>Adicionar ao carrinho</span>
@@ -56,10 +57,17 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Home);
